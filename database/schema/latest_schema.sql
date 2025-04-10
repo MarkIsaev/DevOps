cd3c0c7b7393b0ee378d24de9be42482cd8111bc
Mark Isaev <100965462+MarkIsaev@users.noreply.github.com>
2025-04-10 19:05:57 +0300

    Update latest_schema.sql

--- database/schema/latest_schema.sql
+++ database/schema/latest_schema.sql
@@ -1,37 +1,40 @@
-CREATE TABLE user (
-	id INTEGER NOT NULL, 
-	username VARCHAR(150) NOT NULL, 
-	email VARCHAR(150) NOT NULL, 
-	password VARCHAR(200) NOT NULL, 
-	role VARCHAR(50) NOT NULL, 
-	date_created TIMESTAMP, 
-	PRIMARY KEY (id), 
-	UNIQUE (username), 
-	UNIQUE (email)
+CREATE TABLE "user" (
+    id integer NOT NULL,
+    username varchar(150) NOT NULL,
+    email varchar(150) NOT NULL,
+    password varchar(200) NOT NULL,
+    role varchar(50) NOT NULL,
+    date_created timestamp,
+    PRIMARY KEY (id),
+    UNIQUE (username),
+    UNIQUE (email)
 );
-CREATE TABLE category (
-	id INTEGER NOT NULL, 
-	name VARCHAR(100) NOT NULL, 
-	user_id INTEGER, 
-	PRIMARY KEY (id), 
-	UNIQUE (name), 
-	FOREIGN KEY(user_id) REFERENCES user (id)
+
+CREATE TABLE "category" (
+    id integer NOT NULL,
+    name varchar(100) NOT NULL,
+    user_id integer,
+    PRIMARY KEY (id),
+    UNIQUE (name),
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
-CREATE TABLE note (
-	id INTEGER NOT NULL, 
-	title VARCHAR(200) NOT NULL, 
-	content TEXT NOT NULL, 
-	timestamp DATETIME, 
-	user_id INTEGER NOT NULL, 
-	category_id INTEGER, 
-	PRIMARY KEY (id), 
-	FOREIGN KEY(user_id) REFERENCES user (id), 
-	FOREIGN KEY(category_id) REFERENCES category (id)
+
+CREATE TABLE "note" (
+    id integer NOT NULL,
+    title varchar(200) NOT NULL,
+    content text NOT NULL,
+    timestamp timestamp,
+    user_id integer NOT NULL,
+    category_id integer,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES "user" (id),
+    FOREIGN KEY (category_id) REFERENCES "category" (id)
 );
-CREATE TABLE shared_notes (
-	note_id INTEGER NOT NULL, 
-	user_id INTEGER NOT NULL, 
-	PRIMARY KEY (note_id, user_id), 
-	FOREIGN KEY(note_id) REFERENCES note (id), 
-	FOREIGN KEY(user_id) REFERENCES user (id)
+
+CREATE TABLE "shared_notes" (
+    note_id integer NOT NULL,
+    user_id integer NOT NULL,
+    PRIMARY KEY (note_id, user_id),
+    FOREIGN KEY (note_id) REFERENCES "note" (id),
+    FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
